3977b965e669d1bba1496c8d69150355ade43680
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-24 12:31:46 +0530

    Update connect.py

--- plugins/connect.py
+++ plugins/connect.py
@@ -1,6 +1,18 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# This code belongs to anmol0700,  
+# a passionate developer dedicated to  
+# creating innovative solutions and tools.  
+
+# For more updates and projects,  
+# please visit: t.me/anmol0700.  
+
+# Your support is greatly appreciated,  
+# and it motivates continuous improvement.  
+
+# Feel free to reach out with feedback,  
+# or to collaborate on exciting ideas.  
+
+# Together, we can build amazing things!  
+# Thank you for being a part of this journey! 
 
 from info import *
 from utils import *
@@ -125,3 +137,19 @@ async def connections(bot, message):
           await message.reply(f"❌ <b>Error in FSub</b> (`{f_sub}`)\n`{e}`")
    
     await message.reply(text=text, disable_web_page_preview=True)
+
+# This code belongs to anmol0700,  
+# a passionate developer dedicated to  
+# creating innovative solutions and tools.  
+
+# For more updates and projects,  
+# please visit: t.me/anmol0700.  
+
+# Your support is greatly appreciated,  
+# and it motivates continuous improvement.  
+
+# Feel free to reach out with feedback,  
+# or to collaborate on exciting ideas.  
+
+# Together, we can build amazing things!  
+# Thank you for being a part of this journey! 
