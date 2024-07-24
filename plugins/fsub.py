48f112878058c2783413527a8d42805ca55cecb9
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-24 12:32:35 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -93,7 +93,7 @@ async def f_sub_callback(bot, update):
     try:
        await bot.get_chat_member(f_sub, user_id)          
     except UserNotParticipant:
-       await update.answer("<b>I like your smartness..\nBut don't be over smart</b> 🤭", show_alert=True) # @kingvj01 😁
+       await update.answer("<b>I like your smartness..\nBut don't be over smart</b> 🤭", show_alert=True)
     except:       
        await bot.restrict_chat_member(chat_id=update.message.chat.id, 
                                       user_id=user_id,
