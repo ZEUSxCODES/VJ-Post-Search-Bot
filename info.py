f80c445b616bb63b2f66f794968840d1883e665c
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-24 12:28:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,18 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# This code belongs to anmol0700,  
+# a passionate developer dedicated to  
+# creating innovative solutions and tools.  
 
-# Never Give DATABASE_URI And BOT_TOKEN Here If Repo Is Forked Or Public Because This May Lead Account Ban.
+# For more updates and projects,  
+# please visit: t.me/anmol0700.  
+
+# Your support is greatly appreciated,  
+# and it motivates continuous improvement.  
+
+# Feel free to reach out with feedback,  
+# or to collaborate on exciting ideas.  
+
+# Together, we can build amazing things!  
+# Thank you for being a part of this journey! 
 
 import os
 from os import environ
@@ -13,4 +23,20 @@ BOT_TOKEN    = environ.get("BOT_TOKEN", "")
 DATABASE_URI = environ.get("DATABASE_URI", "")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
 ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+CHANNEL      = environ.get("CHANNEL", "@Hara_Tg")
+
+# This code belongs to anmol0700,  
+# a passionate developer dedicated to  
+# creating innovative solutions and tools.  
+
+# For more updates and projects,  
+# please visit: t.me/anmol0700.  
+
+# Your support is greatly appreciated,  
+# and it motivates continuous improvement.  
+
+# Feel free to reach out with feedback,  
+# or to collaborate on exciting ideas.  
+
+# Together, we can build amazing things!  
+# Thank you for being a part of this journey! 
