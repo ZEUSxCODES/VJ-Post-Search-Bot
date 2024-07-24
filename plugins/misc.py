5471e00c351c5eef1a9e23053a2dba9987e82352
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-24 13:31:02 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -20,10 +20,19 @@ from pyrogram import Client, filters
 from plugins.generate import database 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
-@Client.on_message(filters.command("start") & ~filters.channel)
-async def start(bot, message):
+async def handle_force_subscription(client, message):
+    Fsub = await ForceSub(client, message)
+    if Fsub == 400:
+        return False
+    return True
+
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+    if not await handle_force_subscription(client, message):
+        return
+
     database.insert_one({"chat_id": message.from_user.id})
-    username = (await bot.get_me()).username
+    username = (await client.get_me()).username
     await add_user(message.from_user.id, message.from_user.first_name)
     button = [[
         InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{username}?startgroup=true')
@@ -34,64 +43,94 @@ async def start(bot, message):
         InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
         InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
     ]]
-    await message.reply(text=script.START.format(message.from_user.mention),
-                        disable_web_page_preview=True,
-                        reply_markup=InlineKeyboardMarkup(button))
- 
-@Client.on_message(filters.command("help"))
-async def help(bot, message):
-    await message.reply(text=script.HELP, 
-                        disable_web_page_preview=True)
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=random.choice(PICS),
+        caption=script.START.format(message.from_user.mention),
+        reply_markup=InlineKeyboardMarkup(button)
+    )
 
-@Client.on_message(filters.command("about"))
-async def about(bot, message):
-    await message.reply(text=script.ABOUT.format((await bot.get_me()).mention), 
-                        disable_web_page_preview=True)
+@Client.on_message(filters.command("help") & filters.private)
+async def help(client, message):
+    if not await handle_force_subscription(client, message):
+        return
 
-@Client.on_message(filters.command("stats"))
-async def stats(bot, message):
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=random.choice(PICS),
+        caption=script.HELP,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Back", callback_data="misc_home")]])
+    )
+
+@Client.on_message(filters.command("about") & filters.private)
+async def about(client, message):
+    if not await handle_force_subscription(client, message):
+        return
+
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=random.choice(PICS),
+        caption=script.ABOUT.format((await client.get_me()).mention),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Back", callback_data="misc_home")]])
+    )
+
+@Client.on_message(filters.command("stats") & filters.private)
+async def stats(client, message):
+    if not await handle_force_subscription(client, message):
+        return
+
     g_count, g_list = await get_groups()
     u_count, u_list = await get_users()
-    await message.reply(script.STATS.format(u_count, g_count))
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=random.choice(PICS),
+        caption=script.STATS.format(u_count, g_count)
+    )
 
-@Client.on_message(filters.command("id"))
-async def id(bot, message):
+@Client.on_message(filters.command("id") & filters.private)
+async def id(client, message):
+    if not await handle_force_subscription(client, message):
+        return
+
     text = f"Current Chat ID: `{message.chat.id}`\n"
     if message.from_user:
-       text += f"Your ID: `{message.from_user.id}`\n"
+        text += f"Your ID: `{message.from_user.id}`\n"
     if message.reply_to_message:
-       if message.reply_to_message.from_user:
-          text += f"Replied User ID: `{message.reply_to_message.from_user.id}`\n"
-       if message.reply_to_message.forward_from:
-          text += f"Replied Message Forward from User ID: `{message.reply_to_message.forward_from.id}`\n"
-       if message.reply_to_message.forward_from_chat:
-          text += f"Replied Message Forward from Chat ID: `{message.reply_to_message.forward_from_chat.id}\n`"
-    await message.reply(text)
+        if message.reply_to_message.from_user:
+            text += f"Replied User ID: `{message.reply_to_message.from_user.id}`\n"
+        if message.reply_to_message.forward_from:
+            text += f"Replied Message Forward from User ID: `{message.reply_to_message.forward_from.id}`\n"
+        if message.reply_to_message.forward_from_chat:
+            text += f"Replied Message Forward from Chat ID: `{message.reply_to_message.forward_from_chat.id}\n`"
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo=random.choice(PICS),
+        caption=text
+    )
 
 @Client.on_callback_query(filters.regex(r"^misc"))
-async def misc(bot, update):
+async def misc(client, update):
     data = update.data.split("_")[-1]
-    if data=="home":
-       username = (await bot.get_me()).username
-       button = [[
-           InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{username}?startgroup=true')
-       ],[
-           InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
-           InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
-       ],[
-           InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
-           InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
-       ]]
-       await update.message.edit(text=script.START.format(update.from_user.mention),
+    if data == "home":
+        username = (await client.get_me()).username
+        button = [[
+            InlineKeyboardButton('➕ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕', url=f'http://t.me/{username}?startgroup=true')
+        ],[
+            InlineKeyboardButton("ʜᴇʟᴘ", callback_data="misc_help"),
+            InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data="misc_about")
+        ],[
+            InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url="https://t.me/vj_botz"),
+            InlineKeyboardButton("🔍 ɢʀᴏᴜᴘ", url="https://t.me/vj_bot_disscussion")
+        ]]
+        await update.message.edit(text=script.START.format(update.from_user.mention),
                                  disable_web_page_preview=True,
                                  reply_markup=InlineKeyboardMarkup(button))
-    elif data=="help":
-       await update.message.edit(text=script.HELP, 
+    elif data == "help":
+        await update.message.edit(text=script.HELP, 
                                  disable_web_page_preview=True,
                                  reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Back", callback_data="misc_home")]])) 
-
-    elif data=="about":
-        await update.message.edit(text=script.ABOUT.format((await bot.get_me()).mention), 
+    elif data == "about":
+        await update.message.edit(text=script.ABOUT.format((await client.get_me()).mention), 
                                   disable_web_page_preview=True,
                                   reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⬅️ Back", callback_data="misc_home")]]))
 
